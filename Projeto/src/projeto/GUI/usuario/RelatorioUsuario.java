7c06c6acb5315e1591787e01b1992623b753082d
AnaCarolinaNeves <aninha_0512_@hotmail.com>
2022-06-02 11:53:59 -0300

    Tela centralizada

--- Projeto/src/projeto/GUI/usuario/RelatorioUsuario.java
+++ Projeto/src/projeto/GUI/usuario/RelatorioUsuario.java
@@ -181,6 +181,7 @@ public class RelatorioUsuario extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
